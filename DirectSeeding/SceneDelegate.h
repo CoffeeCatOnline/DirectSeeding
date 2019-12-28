d0e3df3caa4b9eaf086137af0976ac5b9f77b714
CoffeeCatOnline <firstdaystudy@163.com>
2019-12-29 00:48:46 +0800

    新建Xcode 11.0空白项目

---
+++ DirectSeeding/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  DirectSeeding
+//
+//  Created by apple on 2019/12/28.
+//  Copyright © 2019 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
